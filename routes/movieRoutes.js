fae5d9ef89d84ecc0ea25a56050dccb46bdcc576
Ch1ef <adammbule98@gmail.com>
2025-02-23 20:04:23 +0300

    vercel unused imports

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,12 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { getTrendingMovies, getmovieImage, discovermovies, searchformovies, addrating } from '../controllers/movieController.js'; // Import as default
+import { getTrendingMovies } from '../controllers/movieController.js'; // Import as default
 import { verifyJWT } from '../middleware/authMiddleware.js';
 
 router.get('/trendingmovies', getTrendingMovies);
-//router.get('/image', getmovieImage);
-//router.get('/discovermovies', discovermovies);
-//router.get('/searchformovie', searchformovies);
-//router.get('/addrating', addrating);
+
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { login, googleLogin, logout } from '../controllers/userController.js'; // Import controller functions
+import { login, googleLogin } from '../controllers/userController.js'; // Import controller functions
 
 const router = express.Router();
 
 //router.post('/signup', createUser);
 router.post('/login', login);
 router.post('/login/google', googleLogin);
-router.post('logout', logout);
 
 export default router; // Correct: Export the router
