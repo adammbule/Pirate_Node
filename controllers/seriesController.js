c040c58a5bb5d8074183cb5325ec7c66875810cf
Ch1ef <adammbule98@gmail.com>
2025-02-24 23:53:57 +0300

    trending series route addition

--- controllers/seriesController.js
+++ controllers/seriesController.js
@@ -2,13 +2,27 @@ import Series from '../models/series.js'; // Import the Series model (with .js e
 
 // Define controller methods
 export const getTrendingSeries = async (req, res) => {
+  const url = 'https://api.themoviedb.org/3/trending/tv/day?language=en-US';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDk4ZDA0NzU0NjI5MDNlODRmMGZmNjAxYjQwZjRhNCIsIm5iZiI6MTcwNTk0MjA4Ny45NDU5OTk5LCJzdWIiOiI2NWFlOWM0NzNlMmVjODAwZWJmMDA3YTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.m-rvfyxU5wUwRy8Z_jypbh2zfqubxpN_OuS8GVaNE48'
+    }
+  };
+
   try {
-    const trendingSeries = await Series.find({ trending: true });
-    res.json(trendingSeries);
-  } catch (error) {
-    console.error("Error fetching trending series:", error); // Improved error logging
-    res.status(500).json({ error: error.message });
-  }
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`HTTP error ${response.status}: ${errorText}`);
+      }
+      const json = await response.json();
+      return res.json(json); // Return the fetched trending movies data
+    } catch (error) {
+      console.error('Error fetching trending series:', error);
+      return res.status(500).json({ message: 'Error fetching series' });
+    }
 };
 
 export default getTrendingSeries;
